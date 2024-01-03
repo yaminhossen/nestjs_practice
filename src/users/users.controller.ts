8452e3b124f33d4925abd1d883025377d8dcf052
Yamin Hossain <streatdot616@gmail.com>
2024-01-03 18:04:06 +0600

    last updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // get all users /users
+    @Get()
+    getUsers() {
+        return ["ljsdfl"];
+    }
+
+    // get single user /users/:id
+    @Get(":id")
+    getOneNinja(@Param('id') id:string) {
+        return {
+            id
+        };
+    }
+
+    // post /users
+    @Post()
+    createUser(){
+        return {}
+    }
+
+    
+    // update /users/:id
+    @Put()
+    UpdateUser(){
+        return {}
+    }
+
+
+    // delete /users/:id
+    @Delete()
+    removeUser(){
+        return {}
+    }
+}
+
