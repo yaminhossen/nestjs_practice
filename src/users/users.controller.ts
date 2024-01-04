e9c5544c84440721714abcfc3acfe773593fe940
Yamin Hossain <streatdot616@gmail.com>
2024-01-04 17:50:18 +0600

    last updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,46 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+ 
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
     // get all users /users
     @Get()
-    getUsers() {
-        return ["ljsdfl"];
+    getUsers(@Query('gun') gun:'star' | 'muster') {
+        return this.usersService.getGun(gun);
     }
+
+    // get all users /users
+    // @Get()
+    // getUsers(){
+    //     return [
+    //       'dljsfjdlsk'
+    //     ];
+    // }
 
     // get single user /users/:id
     @Get(":id")
-    getOneNinja(@Param('id') id:string) {
-        return {
-            id
-        };
+    getOneNinja(@Param('id') id:number) {
+        return this.usersService.getUser(id);
     }
 
     // post /users
     @Post()
-    createUser(){
-        return {}
+    createUser(@Body() createUserDto: CreateUserDto){
+        return  this.usersService.create(createUserDto);
     }
 
     
     // update /users/:id
-    @Put()
-    UpdateUser(){
-        return {}
-    }
+    // @Put()
+    // updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    //     return this.usersService.update(+id, updateUserDto);
+    // }
 
 
     // delete /users/:id
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [
+        {id:1, name:'usera', gun:'star'},
+        {id:2, name:'userb', gun:'muster'}
+    ]
+    
+    getGun(gun?: 'star' | 'muster'){
+        if(gun){
+            return this.users.filter((user) => user.gun === gun);
+        }
+        return this.users;
+    }
+
+    getUser(id: number){
+        const user = this.users.find((user) => user.id === id)
+        if(!user){
+            throw new Error('user not found')
+        }
+        return user;
+    }
+    create(createUserDto: CreateUserDto) {
+        return 'This action adds a new User got it';
+    }
+
+    findAll() {
+        return `This action returns all Users`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} User`;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} User`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} User`;
+    }
+}
